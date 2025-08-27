d56165774df63b68ef16d6245481a8ece5f9a92e
Nisha Kumari <nisha.23b1541076@abes.ac.in>
2025-08-27 23:54:33 +0530

    Add files via upload

---
+++ BitManipulation.java
@@ -0,0 +1,47 @@
+
+
+public class BitManipulation {
+    public static void main(String[] args){
+        //----------------------------------------------------------- System.out.println("-------------------------");
+        // // binary and 
+        // Scanner sc =new Scanner(System.in);
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        // // a&b is like amultiple .
+        // System.out.println(a&b);
+        //-------------------------------------------------------------- System.out.println("------------------------");
+        // binary or also binary XOR.
+        // Scanner sc= new Scanner(System.in);
+        //  int a=sc.nextInt();
+        //  int b=sc.nextInt();
+        //  or is like add -----XOR is different 0,1 then result 1.
+        // System.out.println(a|b);
+        // System.out.println(a^b);
+        // ------------------------------------------------------------------ System.out.println("---------------------");
+        // Scanner sc=new Scanner(System.in);
+        // int a=sc.nextInt();
+        //  compliment~ ->is like 0 to1 and 1 to0
+        // System.out.println(~a);
+        // --------------------------------------------------------------System.out.println("------------------------"); 
+        // Scanner sc=new Scanner(System.in);
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        // // binary left -> a*2^b and right shift oprations -> a/2^b
+        // System.out.println(a<<b);
+        // System.out.println(a>>b);
+        //-------------------------------------------------------------------System.out.println("---------------------");
+        // check whether the given number is odd or even
+        // Scanner sc=new Scanner(System.in);
+        // int a=sc.nextInt();
+        // if lsb is 0 and with1 gives 0 but lsb 1 &with1 gives 1
+        // int c=(a&1);
+        // if(c==0){
+        //     System.out.println("even");
+        // }
+        // else{
+        //     System.out.println("odd");
+        // }
+        // ----------------------------------------------------------- -----System.out.println("---------------------");
+        // basic completed next intermidiate level
+    }
+}
