b5712b36e63a2ec62a30c5c881c678f539de69cc
Nisha Kumari <nisha.23b1541076@abes.ac.in>
2025-08-28 00:04:24 +0530

    Add files via upload

---
+++ BitManipulation2.java
@@ -0,0 +1,38 @@
+
+ public class BitManipulation2{
+    // // getIthBit---------------------------------------------------------------------------------------------
+    // public static int getIthBit(int n,int i){
+    //     int bitmask=1<<i;
+    //     int c =n & bitmask;
+    //     if(c==0){
+    //         return 0;
+    //     }
+    //     else{
+    //         return 1;
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     System.out.println(getIthBit(3,1));
+    //     -------------------------------------------------System.out.println(-----------------------------------}
+    // public static int setIthBit(int n,int i){
+    //     // 1<<i mean only ith positon par 1 except all 0
+    //     int bitmask=1<<i;
+    //     return (n|bitmask);
+    // }
+    // public static void main(String[] args) {
+    //     // set means fix 1 at ith position
+    //     System.out.println(setIthBit( 10, 2)); 
+    //     System.out.println("------------------------------------------------------------------------------"); 
+   //  public static int clearithbit(int n,int i) {
+   //    return (n&~(1<<i));
+      
+   //  }
+   //  public static void main(String[]args){
+   //    System.out.println(clearithbit(10, 1));
+   //    System.out.println("-----------------------------------------------------------------------------}
+   
+
+
+    }
+    
+
