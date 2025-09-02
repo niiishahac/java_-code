f6a73b170f0127643229bf81c7f867135ed80ea4
Nisha Kumari <ranjusuman1462@gmail.com>
2025-09-02 14:34:35 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,102 @@
+
+// basic code
+public class arraylist{
+    // swap3
+    // public static void swap(ArrayList<Integer> list1,int index1,int index2){
+    //     int temp=list1.get(index1);
+    //     list1.set(index1,list1.get(index2));
+    //     list1.set(index2,temp);
+        
+    // }
+public static void main(String args[]){
+    // ArrayList<Integer>list1=new ArrayList<>();
+    // // Operation 
+    // // add element
+    // list1.add(1);
+    // list1.add(5);
+    // list1.add(4);
+    // list1.add(9);
+    // list1.add(2);
+    // swap----------------
+    // System.out.println(list1);
+    // swap(list1, 1, 3);
+    // System.out.println(list1);
+    // -------------------------------
+
+    // System.out.println(list1);
+    // // Get operation -->get and remove the element at that index
+    // int element=list1.get(2);
+    // System.out.println(element);
+    // System.out.println(list1.remove(2));
+    // // set(index,number at that position)
+    // list1.set(3,6);
+    // ---------------------------------1
+    // System.out.println("array");
+    // for(int i=0;i<list1.size();i++){
+    //     System.out.print(list1.get(i)+" ");
+    // }
+    // System.out.println("reverse array");
+    // for(int i=list1.size()-1;i>=0;i--){
+    //     System.out.print(list1.get(i)+" ");
+    // }
+    // -------------------------------------2
+    // find max in arraylist
+    // int max=Integer.MIN_VALUE;
+    // for(int i=0;i<list1.size();i++){
+    //     if(max<list1.get(i)){
+    //         max=list1.get(i);
+
+    //     }
+
+    // }
+    // System.out.println(max);
+    // sorting an Arraylist---4
+    // assending ....
+    // Collections.sort(list1);
+    // System.out.println(list1);
+    // desending order -->by compartors
+    // Collections.sort(list1,Collections.reverseOrder());
+    // System.out.println(list1);
+    // multi-dimentional arraylist-----------------------------------------
+    // ArrayList<ArrayList<Integer>>mainlist=new ArrayList<>();
+    // ArrayList<Integer>list=new ArrayList<>();
+    // // Operation 
+    // // add element
+    // list.add(1);
+    // list.add(5);
+    // list.add(4);
+    // mainlist.add(list);
+    // ArrayList<Integer>list2=new ArrayList<>();
+    // // Operation 
+    // // add element
+    // list2.add(1);
+    // list2.add(4);
+    // list2.add(6);
+    // mainlist.add(list2);
+    // System.out.println(mainlist);
+    // nested loop
+    // for(int i=0;i<mainlist.size();i++){
+    // ArrayList<Integer>listfinal=mainlist.get(i);
+    // for(int j=0;j<listfinal.size();j++){
+    //     System.out.println(listfinal.get(j)+" ");
+    // }
+    // System.out.println();
+    // --------------------------------
+    // ArrayList<ArrayList<Integer>>mainlist1=new ArrayList<>();
+    // ArrayList<Integer>list8=new ArrayList<>();
+    // ArrayList<Integer>list9=new ArrayList<>();
+    // ArrayList<Integer>list10=new ArrayList<>();
+    // for(int i=1;i<5;i++){
+    //     list8.add(i*1);
+    //     list9.add(i*2);
+    //     list10.add(i*3);
+    // }
+    // mainlist1.add(list8);
+    // mainlist1.add(list9);
+    // mainlist1.add(list10);
+    // System.out.println(mainlist1);
+    
+
+
+}
+}
