7dea40e5c06860753b66c2879d67e10c54a732fe
Nisha Kumari <ranjusuman1462@gmail.com>
2025-09-22 00:08:29 +0530

    Add files via upload

---
+++ opps.java
@@ -0,0 +1,15 @@
+public class opps {
+    public static class students {
+        String name;
+        int rollno;
+        int age;    
+    }
+    public static void main(String[] args) {
+        students x =new students();
+        x.name="nani";
+        x.rollno=19;
+        x.age=20;
+        System.out.println(x.age);
+        
+    }
+}
